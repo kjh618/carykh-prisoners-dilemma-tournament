a1949a5d16a2b08c92fda0f0e0b4582b1a331589
Jihoon Kim <kjh618@kaist.ac.kr>
2021-05-26 20:07:20 +0900

    Add reinitiating TFT. Remove print statements

--- code/strats/kjhDetectRandom.py
+++ code/strats/kjhDetectRandom.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 def strategy(history, memory):
-    turn = len(history[0])
-    # BAR = '-' * 20
-    # print(f'{BAR} Turn {turn} {BAR}')
+    """Tit-for-tat but detect random and defect."""
 
+    turn = len(history[0])
     if turn == 0:
-        memory = { 'reactions': np.zeros((2, 2)) }
+        memory = {
+            'reactions': np.zeros((2, 2)),
+            'isOpponentRandom': False,
+        }
         return 1, memory
     elif turn == 1:
         return history[1][-1], memory
@@ -16,27 +18,33 @@ def strategy(history, memory):
     myMove = history[0][-2]
     opponentReaction = history[1][-1]
     reactions[myMove][opponentReaction] += 1
-    # print(f'reactions: \n{reactions}')
 
     # Not enough information -> TFT
-    if np.any(reactions < 3):
+    totalReactions = np.sum(reactions, axis=1)
+    if not np.all(totalReactions >= 5):
+        memory['isOpponentRandom'] = False
         return history[1][-1], memory
 
     # Calculate the propotions of the opponent's reactions to my moves.
     reactionProportions = np.full_like(reactions, np.nan)
     for my in [0, 1]:
-        totalReactions = np.sum(reactions[my])
         for opp in [0, 1]:
-            reactionProportions[my][opp] = reactions[my][opp] / totalReactions
-    # print(f'reactionProportions: \n{reactionProportions}')
+            reactionProportions[my][opp] = reactions[my][opp] / totalReactions[my]
     
     # Calculate the standard deviations of the reaction proportions.
     # FYI, the 2 stds are the same and equal to diff / 2 here.
     reactionDeviations = np.std(reactionProportions, axis=0)
-    # print(f'reactionDeviations: \n{reactionDeviations}')
 
-    # Opponent looks like random -> defect
+    # If the opponent looks like random, defect. Otherwise TFT.
     if np.all(reactionDeviations < 0.1):
+        memory['isOpponentRandom'] = True
         return 0, memory
-    else: # Otherwise, TFT
-        return history[1][-1], memory
+    else:
+        if memory['isOpponentRandom']:
+            # Reinitiate TFT
+            memory['isOpponentRandom'] = False
+            return 1, memory
+        else:
+            # Continue TFT
+            memory['isOpponentRandom'] = False
+            return history[1][-1], memory
